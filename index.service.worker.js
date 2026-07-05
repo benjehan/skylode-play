53182f95374f857e74a9a058602d693d52ad383a
Berry 🫐 <ben@minesmart.cloud>
2026-07-05 21:33:56 +0100

    Deploy 9754ee5: Fix freeze on entering placement mode + auto/manual quality system

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783277889|5440944';
+const CACHE_VERSION = '1783283633|7800445';
 /** @type {string} */
 const CACHE_PREFIX = 'SKYLODE-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
