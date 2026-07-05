96ff0c7f103901acc5c7d18f995d528fbef11505
Berry 🫐 <ben@minesmart.cloud>
2026-07-05 19:58:11 +0100

    Deploy 5ca3b65: Show build stamp in UI; deploy script injects git sha as version

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783276324|7534065';
+const CACHE_VERSION = '1783277889|5440944';
 /** @type {string} */
 const CACHE_PREFIX = 'SKYLODE-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
